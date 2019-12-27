d5769c65afe1d9c52a098a23cdcb475e86de8cd8
Agnes <agneslo@koodata.com.tw>
2019-12-27 18:22:01 +0800

    Add WeatherTableViewCell.

---
+++ GoodWeatherMVVM/Custom Cells/WeatherTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTableViewCell.swift
+//  GoodWeatherMVVM
+//
+//  Created by Agnes Lo on 2019/12/27.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+}
