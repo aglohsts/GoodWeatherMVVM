05541ab6466a0b3cf9f062123db5af594671f42d
Agnes <agneslo@koodata.com.tw>
2019-12-27 12:11:59 +0800

    Initial Commit

---
+++ GoodWeatherMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeatherMVVM
+//
+//  Created by Agnes Lo on 2019/12/27.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
