c7e390636767ffe276f7e3d5c32a50e7aa358f4c
Agnes <agneslo@koodata.com.tw>
2019-12-27 18:22:17 +0800

    Show hard coded information in WeatherListTableViewController.

--- GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
+++ GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
@@ -16,4 +16,30 @@ class WeatherListTableViewController: UITableViewController {
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 100
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: WeatherTableViewCell.self), for: indexPath)
+        
+        guard let weatherTableViewCell = cell as? WeatherTableViewCell else { return cell }
+        
+        weatherTableViewCell.cityNameLabel.text = "Houston"
+        
+        weatherTableViewCell.temperatureLabel.text = "70°"
+        
+        return cell
+    }
 }
