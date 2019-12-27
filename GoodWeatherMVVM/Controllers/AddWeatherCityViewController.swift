1f20a93597225b6263b37321dcd780311d5a3009
Agnes <agneslo@koodata.com.tw>
2019-12-27 16:37:43 +0800

    Add AddWeatherCityViewController and WeatherListTableViewController.

---
+++ GoodWeatherMVVM/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeatherMVVM
+//
+//  Created by Agnes Lo on 2019/12/27.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+}
---
+++ GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeatherMVVM
+//
+//  Created by Agnes Lo on 2019/12/27.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
