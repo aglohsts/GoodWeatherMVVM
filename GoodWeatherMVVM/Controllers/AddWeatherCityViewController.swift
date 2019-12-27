5a6fcc7e664e00738da9cad5b64024d2d8472349
Agnes <agneslo@koodata.com.tw>
2019-12-27 18:29:24 +0800

    Add buttons and textField in AddWeatherCityViewController.

--- GoodWeatherMVVM/Controllers/AddWeatherCityViewController.swift
+++ GoodWeatherMVVM/Controllers/AddWeatherCityViewController.swift
@@ -11,4 +11,14 @@ import UIKit
 
 class AddWeatherCityViewController: UIViewController {
     
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBAction func saveCityButtonPressed() {
+        
+    }
+    
+    @IBAction func close() {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 }
