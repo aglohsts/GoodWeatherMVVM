39253942b752be3ce02861d5262e5db6cf5682a3
Agnes <agneslo@koodata.com.tw>
2020-01-02 16:30:40 +0800

    Add WeatherListViewModelTests.

---
+++ GoodWeatherMVVMTests/WeatherListViewModelTests.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherListViewModelTests.swift
+//  GoodWeatherMVVMTests
+//
+//  Created by Agnes Lo on 2020/1/2.
+//  Copyright © 2020 Agnes Lo. All rights reserved.
+//
+
+import XCTest
+@testable import GoodWeatherMVVM
+
+class WeatherListViewModelTests: XCTestCase {
+    
+    private var weatherListVM: WeatherListViewModel?
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.weatherListVM = WeatherListViewModel()
+        
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+
+}
